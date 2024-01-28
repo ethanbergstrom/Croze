5d165578045884e5c7662d593904039ba51261e2
ethanbergstrom <ethan.bergstrom@gmail.com>
2024-01-28 09:20:42 -0600

    Require specifying package type

--- src/Croze.ps1
+++ src/Croze.ps1
@@ -6,28 +6,52 @@ $BaseParameters = @()
 
 $BaseOutputHandlers = @{
     ParameterSetName = 'Default'
-    Handler = {
+    Handler          = {
         param ( $output )
     }
 }
 
-$PackageInstallHandlers = @{
-    ParameterSetName = 'Default'
-    Handler = {
-        param ( $output )
+$PackageGetHandler = {
+    if ($output) {
+        $output | ConvertFrom-StringData -Delimiter ' ' | ForEach-Object {
+            # Brew supports installing multiple versions side-by-side, but instead of listing them as separate rows, it puts multiple versions on the same row. 
+            # To present this package data in a way that's idiomatic to PowerShell, we need to list each version as a separate object:
+            $_.GetEnumerator() | ForEach-Object {
+                $name = $_.Name
+                $_.Value -split ' ' | Select-Object -Property @{
+                    Name       = 'Name'
+                    Expression = { $name }
+                },
+                @{
+                    Name       = 'Version'
+                    Expression = { $_ }
+                }
+            }
+        } | Select-Object Name, Version
+    }
+}
 
-        if ($output -match 'Pouring') {
-            # Formula output - capture package and dependency name and version
+$PackageInstallHandlers = @(
+    @{
+        ParameterSetName = 'Formula'
+        Handler          = {
+            param ( $output )
+
             $output | Select-String 'Pouring (?<name>\S+)(?<=\w)(-+)(?<version>\d+\.{0,1}\d*\.(?=\d)\d*)' | ForEach-Object -MemberName Matches | ForEach-Object {
-                $match = ($_.Groups | Where-Object Name -in 'name','version').Value
+                $match = ($_.Groups | Where-Object Name -in 'name', 'version').Value
 
                 [PSCustomObject]@{
-                    Name = $match[0]
+                    Name    = $match[0]
                     Version = $match[1]
                 }
             }
-        } elseif ($output -match 'was successfully') {
-            # Cask output - capture package only
+        }
+    },
+    @{
+        ParameterSetName = 'Cask'
+        Handler          = {
+            param ( $output )
+
             $output | Select-String '(?<name>\S+) was successfully' | ForEach-Object -MemberName Matches | ForEach-Object {
                 $match = ($_.Groups | Where-Object Name -eq 'name').Value
                 [PSCustomObject]@{
@@ -36,10 +60,10 @@ $PackageInstallHandlers = @{
             }
         }
     }
-}
+)
 
 # The general structure of this hashtable is to define noun-level attributes, which are -probably- common across all commands for the same noun, but still allow for customization at more specific verb-level defition for that noun.
-# The following three command attributes have the following order of precedence:
+# The following three command attributes have the following order of precedence:`
 # 	OriginalCommandElements will be MERGED in the order of Noun + Verb + Base
 #		Example: Noun HomebrewSource's element 'source', Verb Register's element 'add', and Base elements are merged to become 'Homebrew source add --limit-output --yes'
 # 	Parameters will be MERGED in the order of Noun + Verb + Base
@@ -50,15 +74,15 @@ $PackageInstallHandlers = @{
 #		Example 2: The native commands for Register-HomebrewSource and Unregister-HomebrewSource don't return any output, and until Crescendo supports error handling by exit codes, a base required default output handler that doesn't do anything can be defined and reused in multiple places.
 $Commands = @(
     @{
-        Noun = 'HomebrewTap'
+        Noun  = 'HomebrewTap'
         Verbs = @(
             @{
-                Verb = 'Get'
-                Description = 'Return Homebrew taps'
+                Verb                    = 'Get'
+                Description             = 'Return Homebrew taps'
                 OriginalCommandElements = @('tap')
-                OutputHandlers = @{
+                OutputHandlers          = @{
                     ParameterSetName = 'Default'
-                    Handler = {
+                    Handler          = {
                         param ($output)
                         if ($output) {
                             $output | ForEach-Object {
@@ -71,33 +95,33 @@ $Commands = @(
                 }
             },
             @{
-                Verb = 'Register'
-                Description = 'Register a new Homebrew tap'
+                Verb                    = 'Register'
+                Description             = 'Register a new Homebrew tap'
                 OriginalCommandElements = @('tap')
-                Parameters = @(
+                Parameters              = @(
                     @{
-                        Name = 'Name'
+                        Name          = 'Name'
                         ParameterType = 'string'
-                        Description = 'Source Name'
-                        Mandatory = $true
+                        Description   = 'Source Name'
+                        Mandatory     = $true
                     },
                     @{
-                        Name = 'Location'
+                        Name          = 'Location'
                         ParameterType = 'string'
-                        Description = 'Source Location'
+                        Description   = 'Source Location'
                     }
                 )
             },
             @{
-                Verb = 'Unregister'
-                Description = 'Unregister an existing Homebrew tap'
-                OriginalCommandElements = @('untap','-f')
-                Parameters = @(
+                Verb                    = 'Unregister'
+                Description             = 'Unregister an existing Homebrew tap'
+                OriginalCommandElements = @('untap', '-f')
+                Parameters              = @(
                     @{
-                        Name = 'Name'
-                        ParameterType = 'string'
-                        Description = 'Source Name'
-                        Mandatory = $true
+                        Name                            = 'Name'
+                        ParameterType                   = 'string'
+                        Description                     = 'Source Name'
+                        Mandatory                       = $true
                         ValueFromPipelineByPropertyName = $true
                     }
                 )
@@ -105,175 +129,137 @@ $Commands = @(
         )
     },
     @{
-        Noun = 'HomebrewPackage'
+        Noun       = 'HomebrewPackage'
         Parameters = @(
             @{
-                Name = 'Name'
-                ParameterType = 'string'
-                Description = 'Package Name'
+                Name                            = 'Name'
+                ParameterType                   = 'string'
+                Description                     = 'Package Name'
                 ValueFromPipelineByPropertyName = $true
+                ParameterSetName                = @('Formula', 'Cask')
             },
             @{
-                Name = 'Formula'
-                OriginalName = '--formula'
-                ParameterType = 'switch'
-                Description = 'Formula'
+                Name             = 'Formula'
+                OriginalName     = '--formula'
+                ParameterType    = 'switch'
+                Description      = 'Formula'
+                ParameterSetName = 'Formula'
             },
             @{
-                Name = 'Cask'
-                OriginalName = '--cask'
-                ParameterType = 'switch'
-                Description = 'Cask'
+                Name             = 'Cask'
+                OriginalName     = '--cask'
+                ParameterType    = 'switch'
+                Description      = 'Cask'
+                ParameterSetName = 'Cask'
             }
         )
-        Verbs = @(
+        Verbs      = @(
             @{
-                Verb = 'Install'
-                Description = 'Install a new package with Homebrew'
+                Verb                    = 'Install'
+                Description             = 'Install a new package with Homebrew'
                 OriginalCommandElements = @('install')
-                OutputHandlers = $PackageInstallHandlers
-                Parameters = @(
+                OutputHandlers          = $PackageInstallHandlers
+                Parameters              = @(
                     @{
-                        Name = 'Force'
-                        OriginalName = '--force'
-                        ParameterType = 'switch'
-                        Description = 'Force'
+                        Name             = 'Force'
+                        OriginalName     = '--force'
+                        ParameterType    = 'switch'
+                        Description      = 'Force'
+                        ParameterSetName = @('Formula', 'Cask')
                     }
                 )
             },
             @{
-                Verb = 'Get'
-                Description = 'Get a list of installed Homebrew packages'
-                OriginalCommandElements = @('list','--versions')
-                OutputHandlers = @{
-                    ParameterSetName = 'Default'
-                    Handler = {
-                        param ( $output )
-
-                        if ($output) {
-                            $output | ConvertFrom-StringData -Delimiter ' ' | ForEach-Object {
-                                # Brew supports installing multiple versions side-by-side, but instead of listing them as separate rows, it puts multiple versions on the same row. 
-                                # To present this package data in a way that's idiomatic to PowerShell, we need to list each version as a separate object:
-                                $_.GetEnumerator() | ForEach-Object {
-                                    $name = $_.Name
-                                    $_.Value -split ' ' | Select-Object -Property @{
-                                        Name = 'Name'
-                                        Expression = {$name}
-                                    },
-                                    @{
-                                        Name = 'Version'
-                                        Expression = {$_}
-                                    }
-                                }
-                            } | Select-Object Name,Version
-                        }
+                Verb                    = 'Get'
+                Description             = 'Get a list of installed Homebrew packages'
+                OriginalCommandElements = @('list', '--versions')
+                OutputHandlers          = @(
+                    @{
+                        ParameterSetName = 'Formula'
+                        Handler          = $PackageGetHandler
+                    },
+                    @{
+                        ParameterSetName = 'Cask'
+                        Handler          = $PackageGetHandler
                     }
-                }
+                )
             },
             @{
-                Verb = 'Find'
-                Description = 'Find a list of available Homebrew packages'
+                Verb                    = 'Find'
+                Description             = 'Find a list of available Homebrew packages'
                 OriginalCommandElements = @('search')
-                OutputHandlers = @{
-                    ParameterSetName = 'Default'
-                    Handler = {
-                        param ( $output )
+                OutputHandlers          = @(
+                    @{
+                        ParameterSetName = 'Formula'
+                        Handler          = {
+                            param ($output)
 
-                        if ($output) {
-
-                            $output | Select-String '==>' | Select-Object -ExpandProperty LineNumber | ForEach-Object {
-                                # The line numbers from Select-Object start at 1 instead of 0
-                                $index = $_ - 1
-                                switch -WildCard ($output[$index]) {
-                                    '*Formulae*' {
-                                        $formulaeStartIndex = $index
-                                    }
-                                    '*Casks*' {
-                                        $casksStartIndex = $index
-                                    }
-                                }   
-                            }
-                            
-                            # Determine the range of formulae output based on whether we also have cask output
-                            $formulaeEndIndex = $(
-                                # Cant use a standard check here, because a valid value could be '0', which would evaluate to $false
-                                if ($formulaeStartIndex -ne $null) {
-                                    if ($casksStartIndex) {
-                                        # Stop capturing formulae output two rows before the start of the Cask index
-                                        $casksStartIndex-2
-                                    }
-                                    else {
-                                        # Capture to the entire output
-                                        $output.Length
-                                    }
-                                }
-                            )
-                            
-                            # Cant use a standard check here, because a valid value could be '0', which would evaluate to $false
-                            if ($formulaeStartIndex -ne $null) {
-                                $output[($formulaeStartIndex+1)..$formulaeEndIndex] | ForEach-Object {
-                                    [PSCustomObject]@{
-                                        Name = $_
-                                        Type = 'Formula'
-                                    }
+                            $output | ForEach-Object {
+                                [PSCustomObject]@{
+                                    Name = $_
+                                    Type = 'Formula'
                                 }
                             }
-                            
-                            if ($casksStartIndex -ne $null) {
-                                $output[($casksStartIndex+1)..($output.Length)] | ForEach-Object {
-                                    [PSCustomObject]@{
-                                        Name = $_
-                                        Type = 'Cask'
-                                    }
+                        }
+                    },
+                    @{
+                        ParameterSetName = 'Cask'
+                        Handler          = {
+                            param ($output)
+
+                            $output | ForEach-Object {
+                                [PSCustomObject]@{
+                                    Name = $_
+                                    Type = 'Cask'
                                 }
                             }
                         }
                     }
-                }
+                )
             },
             @{
-                Verb = 'Update'
-                Description = 'Updates an installed package to the latest version'
+                Verb                    = 'Update'
+                Description             = 'Updates an installed package to the latest version'
                 OriginalCommandElements = @('upgrade')
-                OutputHandlers = $PackageInstallHandlers
+                OutputHandlers          = $PackageInstallHandlers
             },
             @{
-                Verb = 'Uninstall'
-                Description = 'Uninstall an existing package with Homebrew'
+                Verb                    = 'Uninstall'
+                Description             = 'Uninstall an existing package with Homebrew'
                 OriginalCommandElements = @('uninstall')
             }
         )
     },
     @{
-        Noun = 'HomebrewPackageInfo'
+        Noun  = 'HomebrewPackageInfo'
         Verbs = @(
             @{
-                Verb = 'Get'
-                Description = 'Shows information on a specific Homebrew package'
-                OriginalCommandElements = @('info','--json=v2')
-                Parameters = @(
+                Verb                    = 'Get'
+                Description             = 'Shows information on a specific Homebrew package'
+                OriginalCommandElements = @('info', '--json=v2')
+                Parameters              = @(
                     @{
-                        Name = 'Name'
-                        ParameterType = 'string'
-                        Description = 'Package Name'
+                        Name                            = 'Name'
+                        ParameterType                   = 'string'
+                        Description                     = 'Package Name'
                         ValueFromPipelineByPropertyName = $true
                     },
                     @{
-                        Name = 'Formula'
-                        OriginalName = '--formula'
+                        Name          = 'Formula'
+                        OriginalName  = '--formula'
                         ParameterType = 'switch'
-                        Description = 'Formula'
+                        Description   = 'Formula'
                     },
                     @{
-                        Name = 'Cask'
-                        OriginalName = '--cask'
+                        Name          = 'Cask'
+                        OriginalName  = '--cask'
                         ParameterType = 'switch'
-                        Description = 'Cask'
+                        Description   = 'Cask'
                     }
                 )
-                OutputHandlers = @{
+                OutputHandlers          = @{
                     ParameterSetName = 'Default'
-                    Handler = {
+                    Handler          = {
                         param ( $output )
 
                         $output | ConvertFrom-Json | ForEach-Object {
@@ -286,24 +272,24 @@ $Commands = @(
         )
     }
     @{
-        Noun = 'HomebrewTapInfo'
+        Noun  = 'HomebrewTapInfo'
         Verbs = @(
             @{
-                Verb = 'Get'
-                Description = 'Shows information on a specific Homebrew package'
-                OriginalCommandElements = @('tap-info','--json')
-                Parameters = @(
+                Verb                    = 'Get'
+                Description             = 'Shows information on a specific Homebrew package'
+                OriginalCommandElements = @('tap-info', '--json')
+                Parameters              = @(
                     @{
-                        Name = 'Name'
-                        ParameterType = 'string'
-                        Description = 'Package Name'
+                        Name                            = 'Name'
+                        ParameterType                   = 'string'
+                        Description                     = 'Package Name'
                         ValueFromPipelineByPropertyName = $true
                     }
                 )
                 DefaultParameterSetName = 'Default'
-                OutputHandlers = @{
+                OutputHandlers          = @{
                     ParameterSetName = 'Default'
-                    Handler = {
+                    Handler          = {
                         param ( $output )
 
                         $output | ConvertFrom-Json
